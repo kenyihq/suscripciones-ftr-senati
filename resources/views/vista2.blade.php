596f0b1324face04fd30c0db02eed4eb8e2580a5
Kenyi Hancco <kenyihq@gmail.com>
2022-04-22 09:15:18 -0500

    Se agrego la vista 2

---
+++ resources/views/vista2.blade.php
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h2>Hola, soy la vista 2</h2>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('vista1');
+    return view('vista2');
 });
 
 Auth::routes();
